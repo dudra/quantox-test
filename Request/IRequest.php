dcd6ca409034944562b06862424306e699bf4b3b
Dusan Dragas <dudra@live.com>
2018-09-08 20:01:44 +0200

    basic router

---
+++ Request/IRequest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dulo
+ * Date: 9/8/18
+ * Time: 6:38 PM
+ */
+
+interface IRequest {
+
+	public function getBody();
+
+}
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dulo
+ * Date: 9/8/18
+ * Time: 6:49 PM
+ */
+include_once 'Request/Request.php';
+include_once 'Router/Router.php';
+
+$router = new Router(new Request);
+
+$router->get('/', function() {
+	return <<<HTML
+  <h1>Home</h1>
+HTML;
+});
+
+$router->get('/profile', function($request) {
+	return <<<HTML
+  <h1>Profile</h1>
+HTML;
+});
+
+$router->post('/data', function($request) {
+	return json_encode($request->getBody());
+});
