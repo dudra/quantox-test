9fa971378f07a82aeb0685415956121b448e1439
Dusan Dragas <dudra@live.com>
2018-09-08 21:24:58 +0200

    Basic controller - test

---
+++ Controllers/EventController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dulo
+ * Date: 9/8/18
+ * Time: 8:13 PM
+ */
+
+class EventController {
+
+	public function __construct() {
+
+	}
+
+	public static function getEvents() {
+		return 'Radi!';
+	}
+
+}
--- index.php
+++ index.php
@@ -7,6 +7,7 @@
  */
 include_once 'Request/Request.php';
 include_once 'Router/Router.php';
+include_once 'Controllers/EventController.php';
 
 $router = new Router(new Request);
 
@@ -15,6 +16,8 @@ $router->get('/', function() {
   <h1>Home</h1>
 HTML;
 });
+
+$router->get('/get', EventController::getEvents());
 
 $router->get('/profile', function($request) {
 	return <<<HTML
