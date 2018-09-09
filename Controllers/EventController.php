668d5f8d8bcb007a81736d63aaa0a4b1b87fab49
Dusan Dragas <dudra@live.com>
2018-09-09 23:06:33 +0200

    save xml logic

--- Controllers/EventController.php
+++ Controllers/EventController.php
@@ -24,15 +24,18 @@ class EventController {
 			switch ($_GET['p']) {
 				case 'json':
 					http_response_code(200);
-
+					$data = EventRepo::getEvents();
+					return json_encode($data);
 					break;
 				case 'csv':
 					http_response_code(200);
-
+					$data = EventRepo::getEvents();
+					EventController::exportToCsv($data);
 					break;
 				case 'xml':
 					http_response_code(200);
-
+					$data = EventRepo::getEvents();
+					EventController::exportToXML($data);
 					break;
 				default:
 					http_response_code(400);
@@ -48,22 +51,68 @@ class EventController {
 	public static function saveEvent($request) {
 
 		$body = $request->getBody();
+		$date = date('Y-m-d');
 
-		if(!isset($body['country']) || !isset($body['event'])){
+		if(!isset($body['country']) || !isset($body['event']) || ($body['event'] != 'view' && $body['event'] != 'play' && $body['event'] != 'click' ) ){
 			http_response_code(400);
 			return json_encode([
 				'message' => 'Bad request!',
 				'code'  => http_response_code()
 			]);
 		}else{
-			$recordExists = EventRepo::checkIfRecordExists($body['country'], $body['event']);
+			$recordExists = EventRepo::checkIfRecordExists($body['country'], $body['event'], $date);
 			if(!$recordExists){
-				EventRepo::saveEvent($body['country'], $body['event']);
+				EventRepo::saveEvent($body['country'], $body['event'], $date);
 			}else{
-
+				EventRepo::incrementEventCounter($body['country'], $body['event'], $date);
 			}
 
 		}
+	}
+
+	private static function exportToCsv($data) {
+		header('Content-Type: text/csv; charset=utf-8');
+		header('Content-Disposition: attachment; filename=data.csv');
+		$output = fopen("php://output", "w");
+		fputcsv($output, array('country', 'event_name', 'SUM'));
+		foreach ($data as $row) {
+			fputcsv($output, $row);
+		}
+		fclose($output);
+	}
+
+	private static function exportToXML($data) {
+
+		$filePath = 'data.xml';
+
+		$dom     = new DOMDocument('1.0', 'utf-8');
+
+		$root      = $dom->createElement('events');
+
+		for($i=0; $i<count($data); $i++){
+
+			$country        =  $data[$i]['country'];
+
+			$eventName      =  $data[$i]['event_name'];
+
+			$sum            =  $data[$i]['SUM(t2.counter)'];
+
+			$rowDom = $dom->createElement('row');
+			$countryDom = $dom->createElement('country', $country);
+			$eventNameDom = $dom->createElement('event-name', $eventName);
+			$sumDom = $dom->createElement('sum', $sum);
+
+			$root->appendChild($rowDom);
+			$rowDom->appendChild($countryDom);
+			$rowDom->appendChild($eventNameDom);
+			$rowDom->appendChild($sumDom);
+
+		}
+
+		$dom->appendChild($root);
+
+		$dom->save($filePath);
+
 	}
 
 }
