bc8ac12c2e672f26210b6389ef1e2effed78fda2
Dusan Dragas <dudra@live.com>
2018-09-09 12:38:32 +0200

    Connection to DB

---
+++ Database/DbConnection.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dulo
+ * Date: 9/9/18
+ * Time: 12:34 AM
+ */
+require_once 'SystemComponents.php';
+
+class DbConnection extends SystemComponents {
+
+	private static $instance = null;
+	private $conn;
+
+	private function __construct($host, $dbName, $username, $password)
+	{
+
+		try
+		{
+			$this->conn = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
+			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		}catch (PDOException $e)
+		{
+			echo $e->getMessage();
+			die();
+		}
+	}
+
+	public static function connectToDb()
+	{
+		$settings = SystemComponents::getSettings();
+
+		if(DbConnection::$instance == null)
+		{
+			DbConnection::$instance = new DbConnection($settings['dbhost'], $settings['dbname'], $settings['dbusername'], $settings['dbpassword']);
+		}
+
+		return DbConnection::$instance->getConn();
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getConn() {
+		return $this->conn;
+	}
+}
---
+++ Database/SystemComponents.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dulo
+ * Date: 9/9/18
+ * Time: 12:48 AM
+ */
+
+abstract class SystemComponents {
+
+	protected $settings;
+
+	public static function getSettings()
+	{
+		$settings['dbhost'] = 'localhost';
+		$settings['dbusername'] = 'root';
+		$settings['dbpassword'] = '';
+		$settings['dbname'] = 'quantox-test';
+
+		return $settings;
+	}
+
+}
